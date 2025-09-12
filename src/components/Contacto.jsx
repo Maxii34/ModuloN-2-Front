07aa162ebb0746ea3ac307d67ff51d5f24a1e3e2
Paula Gramajo <paulagramajo.tuc@gmail.com>
2025-09-11 23:32:14 -0300

    agregue formulario de contacto y algunos estilos

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -4,16 +4,25 @@ const Contacto = () => {
     return (
         <section className="py-3">
       <h1 className="py-2 text-center base-footer text-light">Contacto</h1>
-      <article className="container bg-secondary">
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+      <article className="container bg-body-secondary ">
+        <Form className='p-2'>
+          <Form.Group className="mb-3 w-25" controlId="exampleForm.ControlInput1">
+            <Form.Label>Nombre y Apellido</Form.Label>
+            <Form.Control type="text" placeholder="ej: juan perez" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
+          <Form.Group className="mb-3 w-25" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="ej: juan_perez@gmail.com" />
+          </Form.Group>
+          <Form.Group className="mb-3 w-25" controlId="exampleForm.ControlInput1">
+            <Form.Label>Télefono</Form.Label>
+            <Form.Control type="tel" placeholder="ej: 381-1115469" />
+          </Form.Group>
+          <Form.Group className="mb-3 w-25" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Consulta</Form.Label>
             <Form.Control as="textarea" rows={3} />
           </Form.Group>
+          <Button className='btn btn-success'>Enviar</Button>
         </Form>
       </article>
     </section>
