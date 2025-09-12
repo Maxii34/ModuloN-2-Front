a12f498c04c5a319044dd4e35d5ce9185b7c0d39
Santiago Robledo <santiddf9@gmail.com>
2025-09-11 21:22:21 -0300

    cambie la imagen y le añadi sombras

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,10 +1,12 @@
 import ImagenPagInicio from "../assets/ImagenPagInicio.png";
+import vaca from "../assets/vaca.jpg";
+import Carousel from "react-bootstrap/Carousel";
 
 const Inicio = () => {
   return (
     <>
       <section>
-        <article>
+        <article className="mb-5">
           <img
             src={ImagenPagInicio}
             alt="Imagen de la página de inicio"
