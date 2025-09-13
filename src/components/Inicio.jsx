0eef40a55780b5d3863c6d72961285d95108a688
Santiago Robledo <santiddf9@gmail.com>
2025-09-12 22:08:36 -0300

    agregue una seccion de opiniones

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -4,9 +4,10 @@ import vacunas from "../assets/vacunas.jpg";
 import cirugia from "../assets/cirugia.jpg";
 import ultrasonido from "../assets/ultrasonido.jpg";
 import peluqueria from "../assets/peluqueria.jpg";
-import vaca1 from "../assets/vaca1.jpg";
-import vaca2 from "../assets/vaca2.jpg";
-import { Carousel, Button, Card, Container, Row, Col } from "react-bootstrap";
+import Camila from "../assets/Camila.jpg";
+import Maria from "../assets/Maria.jpg";
+import Lucas from "../assets/Lucas.jpg";
+import Valentina from "../assets/Valentina.jpg";import { Carousel, Button, Card, Container, Row, Col } from "react-bootstrap";
 
 const Inicio = () => {
   return (
@@ -20,9 +21,40 @@ const Inicio = () => {
             className="imgInicio"
           />
         </article>
+        {/* Sobre Dog-Tor */}
+        <article>
+          <Card className="mx-5">
+            <Card.Header>
+              <Card.Title>
+                A cerca de <strong>Dog-Tor</strong>
+              </Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <b>Dog-Tor</b> es una clínica veterinaria comprometida con la
+              salud y bienestar de tu mascota. Brindamos atención integral, que
+              incluye consultas preventivas, vacunación, cirugías menores,
+              diagnóstico clínico y servicios de higiene y estética. Nuestro
+              equipo de profesionales{" "}
+              <b>
+                —Robledo Garrido Santiago Andrés, Joaquín Albornoz Bresla,
+                Maximiliano Ordoñez y Paula Gramajo—
+              </b>{" "}
+              combina experiencia y dedicación para asegurar que cada animal
+              reciba cuidados personalizados y seguros. En Dog-Tor, trabajamos
+              para que tu compañero peludo esté sano, feliz y acompañado en cada
+              etapa de su vida.
+            </Card.Body>
+            <Card.Footer>
+              <Card.Text>
+                Horarios de atención: <b>Lunes a Viernes: 9:00 AM – 8:00 PM</b>
+              </Card.Text>
+            </Card.Footer>
+          </Card>
+        </article>
         {/* carrusel de servicios */}
         <article className="my-3">
           <h1 className="text-center">Nuestros servicios</h1>
+
           <Carousel>
             {/* Card 1 */}
             <Carousel.Item>
@@ -157,6 +189,70 @@ const Inicio = () => {
         {/* Opiniones */}
         <article>
           <h2 className="text-center">Opiniones</h2>
+          <div className="mb-5">
+            <Container>
+              <Row>
+                <Col className="col-12 col-md-6 col-lg-3 mb-3">
+                  <Card className="h-100 ">
+                    <Card.Img variant="top" src={Maria} className="opinionImg"/>
+                    <Card.Body>
+                      <Card.Title className="text-center">
+                        María López
+                      </Card.Title>
+                      <Card.Text>
+                        "Excelente atención y cuidado para mi perro. El equipo
+                        veterinario es muy profesional y amable. ¡Recomendado!"
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col className="col-12 col-md-6 col-lg-3 mb-3">
+                  <Card className="h-100">
+                    <Card.Img variant="top" src={Valentina} className="opinionImg"/>
+                    <Card.Body>
+                      <Card.Title className="text-center">
+                        Valentina Gómez
+                      </Card.Title>
+                      <Card.Text>
+                        "Muy contento con la atención que recibimos. Los
+                        profesionales son amables y se nota su experiencia.
+                        ¡Totalmente recomendable!"
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col className="col-12 col-md-6 col-lg-3 mb-3">
+                  <Card className="h-100">
+                    <Card.Img variant="top" src={Lucas} className="opinionImg"/>
+                    <Card.Body>
+                      <Card.Title className="text-center">
+                        Lucas Fernández
+                      </Card.Title>
+                      <Card.Text>
+                        "Servicio impecable y muy dedicado con mi mascota. El
+                        personal demuestra gran profesionalismo y cariño. ¡Lo
+                        recomiendo!"
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col className="col-12 col-md-6 col-lg-3 mb-3">
+                  <Card className="h-100">
+                    <Card.Img variant="top" src={Camila} className="opinionImg"/>
+                    <Card.Body>
+                      <Card.Title className="text-center">
+                        Camila Rojas
+                      </Card.Title>
+                      <Card.Text>
+                        "Atención de primera para mi perrito. Todo el equipo es
+                        cordial y muy competente. ¡Sin dudas volveré!"
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </div>
         </article>
       </section>
     </>
