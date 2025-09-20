2d4fcf91b0557c339a2a513f0997383e54a48eca
Paula Gramajo <paulagramajo.tuc@gmail.com>
2025-09-20 15:14:01 -0300

    agregue a los miembros del equipo

--- src/components/CardMiembro.jsx
+++ src/components/CardMiembro.jsx
@@ -1,27 +1,41 @@
 import { Card, Col, Button } from "react-bootstrap";
-import Valentina from "../assets/Valentina.jpg"
+import { useState } from "react";
 
-const CardMiembro = () => {
+const CardMiembro = ({ nombre, imagen, github, linkedin }) => {
+  const [error, setError] = useState(false);
+
+  const handleError = () => {
+    setError(true);
+  };
+
   return (
     <Col md={6} lg={3} className="mb-3">
       <Card className="h-100 card-miembro border-success custom-card">
         <div className="p-2">
           <img
-            src={Valentina}
-            alt='valentina'
+            src={error ? 'URL_DE_IMAGEN_DE_PLACEHOLDER' : imagen}
+            alt={nombre}
             className="card-img-miembro"
+            onError={handleError}
           />
         </div>
         <Card.Body className="border-top border-success">
-          <Card.Title className="text-center">Paula Gramajo</Card.Title>
+          <Card.Title className="text-center">{nombre}</Card.Title>
           <div className="d-flex justify-content-evenly mt-3">
-          <Button variant="success" className="px-3"><i class="bi bi-github"></i></Button>
-          <Button variant="success" className="px-3"><i class="bi bi-linkedin"></i></Button>
+            <a href={github} target="_blank" rel="noopener noreferrer">
+              <Button variant="success" className="px-3">
+                <i className="bi bi-github"></i>
+              </Button>
+            </a>
+            <a href={linkedin} target="_blank" rel="noopener noreferrer">
+              <Button variant="success" className="px-3">
+                <i className="bi bi-linkedin"></i>
+              </Button>
+            </a>
           </div>
         </Card.Body>
       </Card>
     </Col>
-    
   );
 };
 
--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -1,21 +1,60 @@
-import CardMiembro from "../CardMiembro";
-import { Container, Form, Row } from "react-bootstrap";
+import CardMiembro from '../CardMiembro';
+import { Container, Row } from 'react-bootstrap';
+import Paula from '../../assets/Paula.jpg';
+import Maximiliano from '../../assets/Maximiliano.jpg';
 
+const miembrosDelEquipo = [
+  {
+    id: 1,
+    nombre: 'Maximiliano Ordoñez',
+    imagen: Maximiliano,
+    github: 'https://github.com/Maxii34',
+    linkedin: 'https://www.linkedin.com/in/maxiiordo%C3%B1ez/',
+  },
+  {
+    id: 2,
+    nombre: 'Paula Gramajo',
+    imagen: Paula,
+    github: 'https://github.com/PaulaGramajo',
+    linkedin: '',
+  },
+  {
+    id: 3,
+    nombre: 'Paula Gramajo',
+    imagen: Paula,
+    github: 'https://github.com/PaulaGramajo',
+    linkedin: '',
+  },
+  {
+    id: 4,
+    nombre: 'Paula Gramajo',
+    imagen: Paula,
+    github: 'https://github.com/PaulaGramajo',
+    linkedin: '',
+  }
+];
 
 const Nosotros = () => {
-    return (
-        <Container className="py-3">
-         <h1 className="text-center py-3">Conocé a nuestro equipo</h1>
-            
-        <Row className="shadow py-4 px-3 rounded-3">
-       
-          <CardMiembro></CardMiembro>
-          <CardMiembro></CardMiembro>
-          <CardMiembro></CardMiembro>
-          <CardMiembro></CardMiembro>
-        </Row>
-        </Container>
-    );
+  return (
+    <Container className='border rounded-4 my-3'>
+      <div className='text-center'>
+        <h1>Conocé a nuestro equipo</h1>
+        <p className='fs-5'>¡Bienvenido a Dog-Tor! Conoce a los creadores de este proyecto. Un grupo de desarrolladores comprometidos con la excelencia y la innovación. Juntos, convertimos ideas en soluciones funcionales y eficientes.</p>
+      </div>
+      
+      <Row className='p-3'>
+        {miembrosDelEquipo.map((miembro) => (
+          <CardMiembro
+            key={miembro.id}
+            nombre={miembro.nombre}
+            imagen={miembro.imagen}
+            github={miembro.github}
+            linkedin={miembro.linkedin}
+          />
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
 export default Nosotros;
