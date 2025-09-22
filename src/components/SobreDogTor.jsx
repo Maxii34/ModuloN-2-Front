55a5ea2f6704b68aae9385a97034d61906c48769
Maxi Ordoñez <exemaxi@outlook.com>
2025-09-22 17:11:13 -0300

    Se mejoro redaccion del texto

--- src/components/SobreDogTor.jsx
+++ src/components/SobreDogTor.jsx
@@ -12,23 +12,20 @@ const SobreDogTor = () => {
           </Card.Header>
           <Card.Body>
             <b>Dog-Tor</b> es una clínica veterinaria comprometida con la salud
-            y bienestar de tu mascota. Brindamos atención integral, que incluye
+            y bienestar de tu mascota. Ofrecemos atención integral, que incluye
             consultas preventivas, vacunación, cirugías menores, diagnóstico
             clínico y servicios de higiene y estética. Nuestro equipo de
-            profesionales
-            <b>
-              —Robledo Garrido Santiago Andrés, Joaquín Albornoz Bresla,
-              Maximiliano Ordoñez y Paula Gramajo—
-            </b>
-            combina experiencia y dedicación para asegurar que cada animal
+            profesionales: 
+            <b> Robledo Garrido Santiago Andrés, Joaquín Albornoz Bresla,
+              Maximiliano Ordoñez y Paula Gramajo. </b>
+            Combina experiencia y dedicación para asegurar que cada mascota
             reciba cuidados personalizados y seguros. En Dog-Tor, trabajamos
             para que tu compañero peludo esté sano, feliz y acompañado en cada
             etapa de su vida.
           </Card.Body>
           <Card.Footer>
             <Card.Text>
-              Horarios de atención:
-              <b>Lunes, Miercoles y Viernes: 9:00 AM – 8:00 PM</b>
+              Horarios de atención: <b>Lunes, Miercoles y Viernes: 9:00 AM – 8:00 PM</b>
             </Card.Text>
           </Card.Footer>
         </Card>
